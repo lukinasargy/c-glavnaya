3b073cdcad4817e56fa9310db3c35f7fad898279
Sargy Lukina <lukina_sargy@mail.ru>
2017-09-03 22:16:41 +0300

    header_start

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,11 @@ gulp.task('img', function () {
     .pipe(newer(dirs.build + '/img'))                       // оставить в потоке только новые файлы (сравниваем с содержимым папки билда)
     .pipe(gulp.dest(dirs.build + '/img'));                  // записываем файлы (путь из константы)
 });
-
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src(dirs.source + '/fonts/**/*.*')
+    .pipe(gulp.dest(dirs.build + '/fonts')); 
+});
 // ЗАДАЧА: Оптимизация изображений (ЗАДАЧА ЗАПУСКАЕТСЯ ТОЛЬКО ВРУЧНУЮ)
 gulp.task('img:opt', function () {
   return gulp.src([
@@ -184,6 +188,7 @@ gulp.task('build', gulp.series(
   'png:sprite',
   gulp.parallel('style', 'img', 'js'),
   'pug',
+  'fonts',
   'html'
 ));
 
